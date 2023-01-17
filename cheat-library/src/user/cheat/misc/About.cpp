95502cd6f9716f06bf6401a7622b2d50d4371f25
Gktwo <1209951416@qq.com>
2023-01-18 02:41:49 +0800

    3.4+
    3.4+

--- cheat-library/src/user/cheat/misc/About.cpp
+++ cheat-library/src/user/cheat/misc/About.cpp
@@ -4,53 +4,53 @@
 #include <helpers.h>
 #include <cheat/game/EntityManager.h>
 
-namespace cheat::feature 
+namespace cheat::feature
 {
-    const FeatureGUIInfo& About::GetGUIInfo() const
-    {
-        static const FeatureGUIInfo info{ "", u8"关于", false };
-        return info;
-    }
+	const FeatureGUIInfo& About::GetGUIInfo() const
+	{
+		static const FeatureGUIInfo info{ "", u8"关于", false };
+		return info;
+	}
 
-    void About::DrawMain()
-    {
-        ImGui::TextColored(ImColor(199, 16, 162, 255), u8"中文更新: ");
-        TextURL(u8"中文qq群", "https://jq.qq.com/?_wv=1027&k=JNXeTQFV", true, false);
+	void About::DrawMain()
+	{
+		ImGui::TextColored(ImColor(199, 16, 162, 255), u8"中文更新: ");
+		TextURL(u8"中文qq群", "https://jq.qq.com/?_wv=1027&k=JNXeTQFV", true, false);
 
-        ImGui::TextColored(ImColor(28, 202, 214, 255), u8"Akebi官网Github:");
-        TextURL(u8"官网Github链接", "https://github.com/Akebi-Group/Akebi-GC", true, false);
+		ImGui::TextColored(ImColor(28, 202, 214, 255), u8"Akebi官网Github:");
+		TextURL(u8"官网Github链接", "https://github.com/Akebi-Group/Akebi-GC", true, false);
 
 		ImGui::TextColored(ImColor(28, 202, 214, 255), u8"Akebi官网discord:");
 		TextURL(u8"官网Discord 邀请链接", "https://discord.com/invite/MmV8hNZB9S", true, false);
 
-        ImGui::Text(u8"中文汉化:");
-        ImGui::SameLine();
-        ImGui::TextColored(ImColor(0, 102, 255, 255), "gktwo,dongfang");
+		ImGui::Text(u8"中文汉化:");
+		ImGui::SameLine();
+		ImGui::TextColored(ImColor(0, 102, 255, 255), "gktwo,dongfang");
 
-        ImGui::Text(u8"创始人:");
-        ImGui::SameLine();
-        ImGui::TextColored(ImColor(0, 102, 255, 255), "Callow");
-        
+		ImGui::Text(u8"创始人:");
+		ImGui::SameLine();
+		ImGui::TextColored(ImColor(0, 102, 255, 255), "Callow");
+
 		ImGui::Text(u8"主要开发者:");
 		ImGui::SameLine();
 		ImGui::TextColored(ImColor(0, 102, 255, 255), "Taiga");
 
 		ImGui::Text(u8"主要贡献者:");
 		ImGui::TextColored(ImColor(0, 102, 255, 255), "RyujinZX, WitchGod, m0nkrel, Shichiha, harlanx, andiabrudan, hellomykami");
-       
-        ImGui::TextColored(ImColor(199, 16, 162, 255), u8"倒卖，转载准则:");
-        ImGui::SameLine();
-        ImGui::TextColored(ImColor(255, 255, 0, 255), u8"软件开源免费，搬运请标明出处，作者，转载前请联系相关作者，禁止一切涉及交易行为");
 
-        ImGui::TextColored(ImColor(199, 16, 162, 255), u8"倒卖，未经允许搬运公示和吐槽板:");
-        ImGui::SameLine();
-        ImGui::TextColored(ImColor(255, 0, 0, 255), u8"偏爱技术论坛（之前我搬运过东方的，这个翻译他有参与过，现在有人说我后来发的也有搬运，\n你这论坛注册掏钱，在我看来和倒卖行为没有区别）。\n有人在某些群说我发的这个是盗版，我的评价是纯纯傻卵发言，我做个汉化，你说的盗版是什么玩意儿，这本来就是开源免费的，\n盗版是个贬义词，没脑子不要贬低别人的付出");
-    }
+		ImGui::TextColored(ImColor(199, 16, 162, 255), u8"倒卖，转载准则:");
+		ImGui::SameLine();
+		ImGui::TextColored(ImColor(255, 255, 0, 255), u8"软件开源免费，搬运请标明出处，作者，转载前请联系相关作者，禁止一切涉及交易行为");
+
+		ImGui::TextColored(ImColor(199, 16, 162, 255), u8"倒卖，未经允许搬运公示和吐槽板:");
 
 
-    About& About::GetInstance()
-    {
-        static About instance;
-        return instance;
-    }
+	}
+
+
+	About& About::GetInstance()
+	{
+		static About instance;
+		return instance;
+	}
 }
